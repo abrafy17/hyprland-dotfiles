18fffc2fd23179d7d74677481e6677001b01843f
Abdul Rafay <ab.rafy3@gmail.com>
2024-04-24 18:20:39 +0500

    adds wallpaper to pictures dir

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
 
-source_dir_copied="$HOME/github/hyprland-dotfiles/.config"
+source_dir="$HOME/github/hyprland-dotfiles/.config"
 
-source_dir_zshrc_copied="$HOME/github/hyprland-dotfiles/.zshrc"
+source_dir_zshrc="$HOME/github/hyprland-dotfiles/.zshrc"
+
+source_dir_wallpaper="$HOME/github/hyprland-dotfiles/wallpaper"
 
 destination_dir_folders="$HOME/.config"
 
 destination_dir_zshrc="$HOME"
 
-cp "$source_dir_zshrc_copied" "$destination_dir_zshrc"
+destination_dir_wallpaper="$HOME/Pictures"
 
-for folder in "$source_dir_copied"/*; do
+cp "$source_dir_zshrc" "$destination_dir_zshrc"
+
+cp -r "$source_dir_wallpaper" "$destination_dir_wallpaper"
+
+for folder in "$source_dir"/*; do
     if [ -d "$folder" ]; then
         folder_name=$(basename "$folder")
         cp -r "$folder" "$destination_dir_folders"
